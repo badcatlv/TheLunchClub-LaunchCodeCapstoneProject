da02cb3fc79cd7226ad3d0c70dae25effc5c8fe3
Colleen Blake <colleenemilyblake@gmail.com>
2024-01-14 17:42:42 -0600

    Add scaffolding with views

---
+++ option4mvc/Models/Cart.cs
@@ -0,0 +1,10 @@
+﻿namespace option4mvc.Models
+{
+    public class Cart
+    {
+        public int CartId { get; set; }
+        public string? ItemName { get; set;}
+        public string? ItemDescription { get; set;} = null;
+        public decimal? ItemPrice { get; set; } = null;
+    }
+}
