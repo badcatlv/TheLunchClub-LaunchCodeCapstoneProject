2eae93a003a44d6a76a5307bf2c48cfb92c72aab
Colleen Blake <colleenemilyblake@gmail.com>
2024-01-15 12:34:36 -0600

    Update Cart model to contain Id only

---
+++ option4mvc/Data/Migrations/20240115182214_CartTableWithIdOnly.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace option4mvc.Data.Migrations
+{
+    public partial class CartTableWithIdOnly : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ItemDescription",
+                table: "Cart");
+
+            migrationBuilder.DropColumn(
+                name: "ItemName",
+                table: "Cart");
+
+            migrationBuilder.DropColumn(
+                name: "ItemPrice",
+                table: "Cart");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ItemDescription",
+                table: "Cart",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ItemName",
+                table: "Cart",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "ItemPrice",
+                table: "Cart",
+                type: "decimal(18,2)",
+                nullable: true);
+        }
+    }
+}
--- option4mvc/Models/Cart.cs
+++ option4mvc/Models/Cart.cs
@@ -3,8 +3,6 @@
     public class Cart
     {
         public int CartId { get; set; }
-        public string? ItemName { get; set;}
-        public string? ItemDescription { get; set;} = null;
-        public decimal? ItemPrice { get; set; } = null;
+        
     }
 }
