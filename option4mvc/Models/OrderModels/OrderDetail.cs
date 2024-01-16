f4433af7704f978eeb7059cccee24995a3c8349f
Colleen Blake <colleenemilyblake@gmail.com>
2024-01-16 12:54:07 -0600

    Add TODO: comments for future properties in order and cart models

--- option4mvc/Models/OrderModels/OrderDetail.cs
+++ option4mvc/Models/OrderModels/OrderDetail.cs
@@ -6,10 +6,12 @@ namespace option4mvc.Models.OrderModels
     {
         public int OrderDetailId { get; set; }
         public int OrderId { get; set; }
-        public int ProductId { get; set; }
+
+        //TODO: Once catalog is establish we will finalize product properties in table
+        /*public int ProductId { get; set; }
         public Product Product { get; set; }
         public int UnitPrice { get; set; }
-        public int Quantity { get; set; }
+        public int Quantity { get; set; }*/
         public List<OrderDetail>? OrderDetails { get; set;}
         public Order? Order { get; set; }
 
